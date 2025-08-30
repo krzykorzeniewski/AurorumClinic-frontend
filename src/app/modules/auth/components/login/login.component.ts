491b885416c4591908db6d3a82c81ace2d72d388
Marcel Kubis <mkubis42@gmail.com>
2025-08-30 20:02:44 +0200

    feat: alert color change

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,14 +1,32 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { MatError, MatFormField, MatInput, MatInputModule, MatLabel } from '@angular/material/input';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  MatError,
+  MatFormField,
+  MatInput,
+  MatInputModule,
+  MatLabel,
+} from '@angular/material/input';
 import { AuthService } from '../../../core/services/auth.service';
 import { UserLoginDataRequest } from '../../../core/models/user.model';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
-import { MatButton, MatButtonModule, MatIconButton } from '@angular/material/button';
+import {
+  MatButton,
+  MatButtonModule,
+  MatIconButton,
+} from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsService } from '../../../core/services/forms.service';
-import { AlertComponent } from '../../../shared/components/alert/alert.component';
+import {
+  AlertComponent,
+  AlertVariant,
+} from '../../../shared/components/alert/alert.component';
 import { NgIf } from '@angular/common';
 
 @Component({
@@ -30,36 +48,40 @@ import { NgIf } from '@angular/common';
     MatButtonModule,
     MatIconModule,
     AlertComponent,
-    NgIf
+    NgIf,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent{
+export class LoginComponent {
   private _authService = inject(AuthService);
   private _formService = inject(FormsService);
   private _router = inject(Router);
   readonly loginForm = this._formService.getLoginForm();
   hidePassword = signal(true);
-  errorMessage = signal('');
+  infoMessage = signal('');
+  variant = signal<AlertVariant>('warning');
 
   constructor() {
     const navigation = this._router.getCurrentNavigation();
     if (navigation?.extras.state && navigation.extras.state['message']) {
-      this.errorMessage.set(navigation.extras.state['message']);
+      this.variant.set(navigation.extras.state['variant']);
+      this.infoMessage.set(navigation.extras.state['message']);
     }
   }
 
-  onLogin(): void{
-    const userData: UserLoginDataRequest = this.loginForm.value as UserLoginDataRequest;
+  onLogin(): void {
+    const userData: UserLoginDataRequest = this.loginForm
+      .value as UserLoginDataRequest;
 
     this._authService.login(userData).subscribe({
       next: () => {
-        void this._router.navigate(["/home"]);
+        void this._router.navigate(['/home']);
       },
       error: (err) => {
-        this.errorMessage.set(err.message);
+        this.variant.set('warning');
+        this.infoMessage.set(err.message);
       },
     });
   }
--- src/app/modules/shared/components/alert/alert.component.ts
+++ src/app/modules/shared/components/alert/alert.component.ts
@@ -1,12 +1,23 @@
 import { Component, input } from '@angular/core';
 
+export type AlertVariant =
+  | 'warning'
+  | 'success'
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'info'
+  | 'light'
+  | 'dark';
+
 @Component({
   selector: 'app-alert',
   standalone: true,
   imports: [],
   templateUrl: './alert.component.html',
-  styleUrl: './alert.component.css'
+  styleUrl: './alert.component.css',
 })
 export class AlertComponent {
-  text = input('');
+  text = input<string>('');
+  variant = input<AlertVariant>('warning');
 }
