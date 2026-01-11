0f9936d33ea3fe12b04f7846a2b84f645b843ba3
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2026-01-11 22:59:59 +0100

    feat: add about-us component, adjust styles globally

---
+++ src/app/modules/core/components/about-us/about-us.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-about-us',
+  imports: [
+    MatIcon
+  ],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.css'
+})
+export class AboutUsComponent {
+
+}
