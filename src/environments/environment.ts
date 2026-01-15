85844be5d423bf0bac93778a59c2431deb46380c
krzykorzeniewski <korzeniewski.krzysztof@proton.me>
2026-01-15 15:30:45 +0100

    chore: change backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://aurorumclinic-backend.onrender.com/api',
+  apiUrl: 'https://api.aurorumclinic.pl/api',
   startTime: '08:00',
   finishTime: '21:00',
   defaultDoctorProfilePicture:
     'https://aurorum-clinic-images.s3.eu-north-1.amazonaws.com/img.png',
-  socketUrl: 'https://aurorumclinic-backend.onrender.com/ws',
+  socketUrl: 'https://api.aurorumclinic.pl/ws',
 };
