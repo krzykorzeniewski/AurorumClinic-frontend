b880c4e04a262136fee5ba1e04d56ef58af81457
Marcel Kubis <mkubis42@gmail.com>
2026-01-16 23:48:35 +0100

    chore: paginator mapping to polish

--- src/app/modules/shared/methods/customPaginator.ts
+++ src/app/modules/shared/methods/customPaginator.ts
@@ -7,5 +7,18 @@ export function customPaginator() {
   paginatorIntl.previousPageLabel = 'Poprzednia';
   paginatorIntl.firstPageLabel = 'Pierwsza';
   paginatorIntl.lastPageLabel = 'Ostatnia';
+
+  paginatorIntl.getRangeLabel = (
+    page: number,
+    pageSize: number,
+    length: number,
+  ) => {
+    if (length === 0 || pageSize === 0) {
+      return `Strona 0 z 0`;
+    }
+    const totalPages = Math.ceil(length / pageSize);
+    const currentPage = page + 1;
+    return `Strona ${currentPage} z ${totalPages}`;
+  };
   return paginatorIntl;
 }
